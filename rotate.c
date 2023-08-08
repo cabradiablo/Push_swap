54d8de14e93a70e4de999121f5ff5cde80fba028
cabradiablo <alejandropintosalcarazo@gmail.com>
2023-08-08 20:26:14 +0200

    pasar norminette y checker

--- rotate.c
+++ rotate.c
@@ -1,12 +1,41 @@
 #include "pushswap.h"
 
+
 void ft_rotate(t_stack **stack, char x)
+{
+    t_stack *first_node;
+    t_stack *second_node;
+    t_stack *last_node;
+    
+    first_node = *stack;
+    if (first_node == NULL || first_node->next == NULL)
+        return;
+    second_node = first_node->next;
+    last_node = second_node;
+    while (last_node->next != NULL)
+        last_node = last_node->next;
+    first_node->next = NULL;
+    second_node->prev = NULL;
+    last_node->next = *stack;
+    (*stack)->prev = last_node;
+    *stack = second_node;
+    if (x == 'a') {
+        write(1, "ra\n", 3);
+    } else if (x == 'b') {
+        write(1, "rb\n", 3);
+    } else if (x == 'r') {
+        // ...
+    } else {
+        ft_error("ERROR: BAD STACK\n");
+    }
+}
+
+void    ft_reverse_rotate(t_stack **stack, char x)
 {
     t_stack *aux;
     
     if (*stack == NULL || (*stack)->next == NULL)
         return ;
-    //ft_error("ERROR: EMPTY STACKddddd\n");
     aux = *stack;
     while (aux->next)
         aux = aux->next;
@@ -16,32 +45,7 @@ void ft_rotate(t_stack **stack, char x)
     (*stack)->prev = aux;
     aux->prev = NULL;
     *stack = aux;
-    if (x == 'a')
-        write(1, "ra\n", 3);
-    else if (x == 'b')
-        write(1, "rb\n", 3);
-    else if (x =='r')
-        return ;
-    else
-        ft_error("ERROR: BAD STACK\n");
-}
 
-void    ft_reverse_rotate(t_stack **stack, char x)
-{
-    t_stack *aux;
-    
-    if (*stack == NULL || (*stack)->next == NULL)
-        return ;
-    //ft_error("ERROR: EMPTY STACKccc\n");
-    aux = *stack;
-    while (aux->next != NULL)
-        aux = aux->next;
-    if (aux->prev)
-        aux->prev->next = NULL;
-    aux->prev = NULL;
-    (*stack)->prev = aux;
-    aux->next = *stack;
-    *stack = aux;
     if (x == 'a')
         write(1, "rra\n", 4);
     else if (x == 'b')
